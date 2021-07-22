2a6330c91b6e9f5ebca99b8333b5c6eee0670172
Kaiden Howard <londonh.dev@gmail.com>
2021-07-21 19:07:20 -0700

    Texture GUI
    Added texture GUI

---
+++ src/Assets/Assets/Scripts/UI/TextureButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextureButtonScript : MonoBehaviour
+{
+    private void Update()
+    {
+        if (PlayerPrefs.GetString("Texture", "Default") == base.GetComponent<Text>().text)
+        {
+            base.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
+            return;
+        }
+        base.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+    }
+
+    // Token: 0x060000C1 RID: 193 RVA: 0x00006714 File Offset: 0x00004914
+    public void OnClick()
+    {
+        PlayerPrefs.SetString("Texture", base.GetComponent<Text>().text);
+        TextureLoader.LoadTextureSet(base.GetComponent<Text>().text);
+    }
+}
