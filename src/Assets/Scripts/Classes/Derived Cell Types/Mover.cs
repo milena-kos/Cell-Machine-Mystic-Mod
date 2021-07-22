75158a6fbbbf93a937e3aa5e26afa33d682683bb
SnazzGass <45057060+SnazzGass@users.noreply.github.com>
2021-07-22 15:20:38 -0700

    movers are now trash movers
    movers delete the cells in front of them, and have a new texture

--- src/Assets/Scripts/Classes/Derived Cell Types/Mover.cs
+++ src/Assets/Scripts/Classes/Derived Cell Types/Mover.cs
@@ -6,6 +6,43 @@ public class Mover : TrackedCell
 {
     public override void Step()
     {
+
+        int offsetX = 0;
+        int offsetY = 0;
+
+        switch (this.getDirection())
+        {
+            case (Direction_e.RIGHT):
+                offsetX += 1;
+                break;
+            case (Direction_e.DOWN):
+                offsetY += -1;
+                break;
+            case (Direction_e.LEFT):
+                offsetX += -1;
+                break;
+            case (Direction_e.UP):
+                offsetY += 1;
+                break;
+        }
+        //Array index error prevention
+        if (this.position.x - offsetX < 0 || this.position.y - offsetY < 0)
+            return;
+        if (this.position.x - offsetX >= CellFunctions.gridWidth || this.position.y - offsetY >= CellFunctions.gridHeight)
+            return;
+        if (this.position.x + offsetX < 0 || this.position.y + offsetY < 0)
+            return;
+        if (this.position.x + offsetX >= CellFunctions.gridWidth || this.position.y + offsetY >= CellFunctions.gridHeight)
+            return;
+
+        //If there is a cell in our way, delete it :3
+        Cell nextCell = CellFunctions.cellGrid[(int)this.position.x + offsetX, (int)this.position.y + offsetY];
+        if (nextCell != null)
+        {
+            nextCell.Delete(nextCell.generated);
+            AudioManager.i.PlaySound(GameAssets.i.destroy);
+        }
+
         this.Push(this.getDirection(), 0);
         //Suppressed will get set to true so we have to reset it.
         this.suppresed = false;
